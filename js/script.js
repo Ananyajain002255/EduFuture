ae069ba4a2d7b9578cc1869456df5fdf3158e6bb
Ananyajain002255 <ananyajain0074@gmail.com>
2026-07-17 14:22:03 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -40,3 +40,83 @@ counters.forEach(counter => {
     updateCounter();
 
 });
+
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+
+    navLinks.classList.toggle("show");
+
+});
+
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 80){
+
+        navbar.style.background = "rgba(255,255,255,.95)";
+        navbar.style.boxShadow = "0 8px 25px rgba(0,0,0,.12)";
+
+    }
+
+    else{
+
+        navbar.style.background = "rgba(255,255,255,.15)";
+        navbar.style.boxShadow = "none";
+
+    }
+
+});
+
+const words = [
+    "Quality Education",
+    "Digital Learning",
+    "AI Learning",
+    "Future Skills"
+];
+
+let wordIndex = 0;
+let charIndex = 0;
+let typing = document.getElementById("typing");
+
+function typeWord(){
+
+    if(charIndex < words[wordIndex].length){
+
+        typing.textContent += words[wordIndex].charAt(charIndex);
+
+        charIndex++;
+
+        setTimeout(typeWord,100);
+
+    }else{
+
+        setTimeout(deleteWord,1800);
+
+    }
+
+}
+
+function deleteWord(){
+
+    if(charIndex > 0){
+
+        typing.textContent = words[wordIndex].substring(0,charIndex-1);
+
+        charIndex--;
+
+        setTimeout(deleteWord,60);
+
+    }else{
+
+        wordIndex = (wordIndex+1)%words.length;
+
+        setTimeout(typeWord,300);
+
+    }
+
+}
+
+typeWord();
