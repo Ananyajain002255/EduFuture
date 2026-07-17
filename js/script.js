b31c613a295efe160d3aa24751bafb4a95865f33
Ananyajain002255 <ananyajain0074@gmail.com>
2026-07-17 14:03:40 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 console.log("EduFuture Loaded Successfully!");
+const themeBtn = document.getElementById("theme-btn");
+
+themeBtn.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark");
+
+});
