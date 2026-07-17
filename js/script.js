e1e8ca7b19b6a109418f6151c02033456c7a71b0
Ananyajain002255 <ananyajain0074@gmail.com>
2026-07-17 14:09:26 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,3 +6,37 @@ themeBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark");
 
 });
+
+/* Animated Counter */
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+
+    const updateCounter = () => {
+
+        const target = +counter.getAttribute("data-target");
+
+        const count = +counter.innerText;
+
+        const increment = target / 150;
+
+        if (count < target) {
+
+            counter.innerText = Math.ceil(count + increment);
+
+            setTimeout(updateCounter,15);
+
+        }
+
+        else{
+
+            counter.innerText = target.toLocaleString() + "+";
+
+        }
+
+    };
+
+    updateCounter();
+
+});
